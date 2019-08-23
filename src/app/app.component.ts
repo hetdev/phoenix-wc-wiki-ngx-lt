3648251518b9766c4afdec2e6c5d61d1d6f45b2f
Hetdev <hetzel30@gmail.com>
2019-08-23 08:00:00 -0500

    changes listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChange} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges, OnDestroy {
   title = 'phoenix-wc-wiki-ngx-lt';
+
+  @Input() token: string;
+
+  constructor() {
+    console.log('token constructor=' + this.token);
+  }
+
+  ngOnInit() {
+    console.log('token ngOnInit=' + this.token);
+    window.addEventListener('phoenix-wc-wiki-ngx-ce-data-sent-test', this.customEventListenerFunction, true);
+  }
+
+  customEventListenerFunction(event) {
+    console.log('testfunc - ' , event.detail);
+  }
+
+  ngOnChanges(changes: {[token: string]: SimpleChange}) {
+    // check the object "changes" for new data
+    console.log('token ngOnChanges=' + this.token);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('changeNameToCustomEvent', this.customEventListenerFunction, true);
+  }
+
 }
